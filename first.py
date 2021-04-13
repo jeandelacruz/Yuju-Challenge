e05e1b59d2cf5b4a78a470faa57916e1bc7105c9
Jeancarlos <jdelacruz@dakeiv.com>
2021-04-13 12:04:03 -0500

    Remove print and change conditional

--- first.py
+++ first.py
@@ -3,8 +3,7 @@ from re import match
 def checkExpression(expression):
     for v in expression:
         if len(expression) <= 100:
-            if len(expression) <= 5000:
-                print(v)
+            if len(v) <= 5000:
                 if match("\{([^()]|())*\}|^\{\(\{\}\[]\)\}$", v):
                     print(1)
                 else:
